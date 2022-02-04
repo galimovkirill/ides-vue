66c3b413b2b864db3e357c33373651e08bed4b42
Kirill Galimov <galimovdev@gmail.com>
2022-02-05 00:34:27 +0200

    feat: add sidebar component

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,10 +1,12 @@
 <template>
     <the-header />
     <main>
+        <the-sidebar />
         <router-view />
     </main>
 </template>
 
 <script lang="ts" setup>
 import TheHeader from "@/components/layout/TheHeader.vue";
+import TheSidebar from "../components/layout/TheSidebar.vue";
 </script>
